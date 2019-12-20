6e6fe0d75c843b7999a68e22506b6546cafbbf66
Josh Prewer <josh.prewer@focusrite.com>
2019-12-20 14:44:16 +0000

    Separate audio into in memory array

--- api/predictor.py
+++ api/predictor.py
@@ -9,6 +9,8 @@ warnings.filterwarnings('ignore')
 
 import flask
 from spleeter.separator import Separator
+from spleeter.audio.adapter import get_default_audio_adapter
+from scipy.io.wavfile import write
 import os
 from flask import Flask, Response, send_file
 app = Flask(__name__)
@@ -30,17 +32,28 @@ def ping():
 
 
 @app.route('/invocations', methods=['POST'])
-def seperate():
+def invocations():
     try:
-        Separator('base_config.json').separate_to_file('audio_example.mp3', 'output', synchronous=False)
+        spearate()
 
-        file_like_object = io.BytesIO()
-        tar = tarfile.open(fileobj=file_like_object, mode='w|')
-        for name in ["/Users/josh.prewer/Projects/spleeter/api/output/audio_example/vocals.wav", "/Users/josh.prewer/Projects/spleeter/api/output/audio_example/piano.wav"]:
-            tar.add(name, os.path.basename(name))
+        return Response(response='{"status": "ok"}', status=200, mimetype='application/json')
+        # for name in ["/Users/josh.prewer/Projects/spleeter/api/output/audio_example/vocals.wav", "/Users/josh.prewer/Projects/spleeter/api/output/audio_example/piano.wav"]:
+        #     tar.add(name, os.path.basename(name))
 
-        return send_file(file_like_object, attachment_filename='response.tar', mimetype='application/x-tar')
+        # return send_file(file_like_object, attachment_filename='response.tar', mimetype='application/x-tar')
     except Exception as e:
         logging.exception(e)
         return Response(response='{"status": "error"}', status=500, mimetype='application/json')
+
+
+def spearate():
+    separator = Separator('base_config.json')
+
+    audio_loader = get_default_audio_adapter()
+    sample_rate = 44100
+    waveform, _ = audio_loader.load('audio_example.mp3', sample_rate=sample_rate)
+
+    # Perform the separation :
+    prediction = separator.separate(waveform)
+    print(prediction["vocals"])
 
