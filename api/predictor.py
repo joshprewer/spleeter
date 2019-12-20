d4f4281420a2a22db5d2896acea05af7563a3d60
Josh Prewer <josh.prewer@focusrite.com>
2019-12-20 14:27:35 +0000

    Save output files to tar in memory

--- api/predictor.py
+++ api/predictor.py
@@ -1,12 +1,16 @@
 import os
 import shutil
 import json
+import tarfile
 import warnings
+import logging
+import io
 warnings.filterwarnings('ignore')
 
 import flask
 from spleeter.separator import Separator
-from flask import Flask, Response
+import os
+from flask import Flask, Response, send_file
 app = Flask(__name__)
 app.debug = True
 
@@ -29,6 +33,14 @@ def ping():
 def seperate():
     try:
         Separator('base_config.json').separate_to_file('audio_example.mp3', 'output', synchronous=False)
-        return Response(response='{"status": "ok"}', status=200, mimetype='application/json')
-    except:
+
+        file_like_object = io.BytesIO()
+        tar = tarfile.open(fileobj=file_like_object, mode='w|')
+        for name in ["/Users/josh.prewer/Projects/spleeter/api/output/audio_example/vocals.wav", "/Users/josh.prewer/Projects/spleeter/api/output/audio_example/piano.wav"]:
+            tar.add(name, os.path.basename(name))
+
+        return send_file(file_like_object, attachment_filename='response.tar', mimetype='application/x-tar')
+    except Exception as e:
+        logging.exception(e)
         return Response(response='{"status": "error"}', status=500, mimetype='application/json')
+
